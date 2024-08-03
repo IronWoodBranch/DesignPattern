d175d1766faab9933ae8a88b9d4a8f90f63d835a
Mol <375753904@qq.com>
2024-08-03 23:54:24 +0900

    State Pattern part1

---
+++ StatePattern/StatePattern/StateEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern
+{
+    public enum StateEnum
+    {
+        SOLD_OUT=0,
+        NO_QUARTER=1,
+        HAS_QUARTER=2,
+        SOLD=3
+    }
+}
